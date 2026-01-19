92d1112ea6e1d77f8613a69a850f3c4f854368ad
Mike Lyons <buzz.github@clearhive.com>
2026-01-19 15:55:06 -0500

    Add touch drag-and-drop support, tests, and jsdom polyfill for elementFromPoint (#4)

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom";
+
+if (!document.elementFromPoint) {
+  Object.defineProperty(document, "elementFromPoint", {
+    value: () => null,
+    configurable: true
+  });
+}
